54f3c0361527abbb3312f51ce99a5a21e156f360
Styve <styvelioumba98@gmail.com>
2020-05-05 14:17:34 +0200

    creation de tb via la cmd php artisan make:migrate create_produits_table puis creation du controller via la cmd php artisan make:controller ProduitController --ressource pour avoir les methodes crud et en fin creation du model via la cmd php artisan make:model Produit

---
+++ app/Http/Controllers/ProduitController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Produit;
+use Illuminate\Http\Request;
+
+class ProduitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $produit=Produit::all();
+        return $produit;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $produit=new Produit();
+        $produit->name=$request->name;
+        $produit->description=$request->description;
+        $produit->prix=$request->prix;
+        $produit->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $produit=Produit::find($id);
+        return $produit;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $produit=Produit::find($id);
+        $produit->name=$request->name;
+        $produit->description=$request->description;
+        $produit->prix=$request->prix;
+        $produit->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $produit=Produit::find($id);
+        $produit->delete();
+    }
+}
---
+++ database/migrations/2020_05_05_110857_create_produit_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProduitTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produits', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',100);
+            $table->text('description')->nullable();
+            $table->float('prix');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produit');
+    }
+}
